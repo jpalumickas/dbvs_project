c2e5ee75ab9504337f4bd178e7c74a504b01a54a
Justas Palumickas <justas@elish.lt>
2012-12-10 10:21:52 +0200

    Removing database configuration file.

--- src/database/DatabaseConfig.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-public class DatabaseConfig
-{
-	public static final String USERNAME		= "username";
-	public static final String PASSWORD		= "password";
-	public static final String SERVER		= "localhost";
-	public static final String DATABASE		= "postgres";
-}
